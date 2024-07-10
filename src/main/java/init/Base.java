f7ce7f6fce66fa09239a23bc7a40e0142922af72
gourav-007 <gourav071295@gmail.com>
2024-07-11 00:40:48 +0530

    added chromeoptions

--- src/main/java/init/Base.java
+++ src/main/java/init/Base.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -13,7 +14,14 @@ public class Base {
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
